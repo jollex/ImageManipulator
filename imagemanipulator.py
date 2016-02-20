d39be2c1fcad00e5580bae4418f71c7e02895ce5
Alex Jolly <alex@alexjolly.me>
2016-02-19 21:59:19 -0500

    Several changes:  - added method that returns a copy of the image  - made box_size and iterations optional values, dynamically populates the    values if they're not passed in  - if a .jpg file is used, copies the file and renames it with .jpeg  - added cropping frames to size of smallest frame  - added making a looping gif and saving it

--- imagemanipulator.py
+++ imagemanipulator.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import argparse, sys, os, random, io
+from shutil import copyfile
 from PIL import Image
+import images2gif
 
 class ImageManipulator(object):
     """ Class for manipulating an Image """
@@ -37,14 +39,15 @@ class ImageManipulator(object):
         switches pairs of sections.
 
         box_size -- The length of each side of each box.
-        ext    -- The extension of the image
+        ext -- The extension of the image
         """
         boxes, new_w, new_h = self.get_boxes_and_new_size(box_size)
         random.shuffle(boxes)
 
         pairs = []
         for i in range(0, len(boxes), 2):
-            pairs.append((boxes[i], boxes[i + 1]))
+            try: pairs.append((boxes[i], boxes[i + 1]))
+            except IndexError: pass
 
         for pair in pairs:
             log("Pair: {}".format(pair))
@@ -93,29 +96,73 @@ class ImageManipulator(object):
         """
         self.image.save(save_path)
 
+    def copy(self):
+        """ Returns a copy of the image """
+        return self.image.copy()
+
 def log(message):
+    """
+    Simple logging method
+
+    message -- The message to be logged"""
+
     if DEBUG:
         print message
 
 if __name__=='__main__':
+    # Parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('image_path', help='Path to the image to manipulate')
-    parser.add_argument('box_size',
-        help='The size of the boxes the picture will be split into', type=int)
-    parser.add_argument('iterations', help='The number of times to run the\
-        manipulator, multiplying box_size by 2 for each iteration', type=int)
-    parser.add_argument('--debug', help='Use flag to print debugging statements\
-        while the script runs', action='store_true')
+    parser.add_argument('-bs', '--box_size', default=2, help='The size of the\
+        boxes the picture will be split into', type=int)
+    parser.add_argument('-i', '--iterations', default=None, help='The number\
+        of times to run the manipulator, multiplying box_size by 2 for each\
+        iteration', type=int)
+    parser.add_argument('-r', '--random', help='Use flag to randomize position\
+        of boxes', action='store_true')
+    parser.add_argument('-d', '--debug', help='Use flag to print debugging\
+        statements while the script runs', action='store_true')
     args = parser.parse_args()
 
-    f, e = os.path.splitext(args.image_path)
-    box_size = args.box_size
-    iterations = args.iterations
     DEBUG = args.debug
-    for i in range(iterations):
+
+    f, e = os.path.splitext(args.image_path)
+    if e.lower() == '.jpg':
+        copyfile(f + e, f + '.jpeg')
+        e = '.jpeg'
+    box_size = args.box_size
+    box_sizes = []
+
+    # Populate list of box_sizes
+    iterations = args.iterations
+    if iterations:
+        for i in range(iterations):
+            box_sizes.append(box_size)
+            box_size *= 2
+    else:
+        image = Image.open(f + e)
+        size = image.size
+        image.close()
+        while box_size * 2 < min(size):
+            box_sizes.append(box_size)
+            box_size *= 2
+
+    # Create frames
+    frames = []
+    for box_size in box_sizes:
         im = ImageManipulator(f + e)
         im.rotate_sections(box_size, [None, Image.ROTATE_90,
             Image.ROTATE_180, Image.ROTATE_270])
-        im.randomize_sections(box_size, e[1:])
-        im.save("{}-{}{}".format(f, box_size, e))
-        box_size *= 2
+        if args.random: im.randomize_sections(box_size, e[1:])
+        frames.append(im.copy())
+
+    # Crop all frames to size of smallest frame
+    min_w = min(map(lambda frame: frame.size[0], frames))
+    min_h = min(map(lambda frame: frame.size[1], frames))
+    frames = map(lambda frame: frame.crop((0, 0, min_w, min_h)), frames)
+
+    # Loop the animation and save it
+    all_but_one = frames[:-1]
+    all_but_one.reverse()
+    images2gif.writeGif("{}{}.gif".format(f, "-random" if args.random else ""), frames + all_but_one)
+
