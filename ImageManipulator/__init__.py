f53e84524dc4abf4740f92a78a146861f27bf28a
Alex Jolly <alex@alexjolly.me>
2016-03-29 21:41:19 -0400

    Removed logging

--- ImageManipulator/__init__.py
+++ ImageManipulator/__init__.py
@@ -3,12 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config')
 
-if not app.debug:
-    from logging import FileHandler
-    file_handler = FileHandler('app.log')
-    file_handler.setLevel(logging.WARNING)
-    app.logger.addHandler(file_handler)
-
 from ImageManipulator import views
 
 if __name__=='__main__'
