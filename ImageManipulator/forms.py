b8fef4d20c85e15dde157d68fdde9420508e3201
Alex Jolly <alex@alexjolly.me>
2016-04-02 20:30:47 -0400

    Added all favicons ever. Started working on showing errors when submitting invalid forms

--- ImageManipulator/forms.py
+++ ImageManipulator/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import Form
-from wtforms import FileField, RadioField, StringField, SelectField, BooleanField, SubmitField, validators
+from wtforms import FileField, RadioField, StringField, SelectField, BooleanField, SubmitField
+from wtforms.validators import StopValidation, DataRequired
 
 import imghdr
 
@@ -12,30 +13,38 @@ class ImageFileRequired(object):
     Author: https://gist.github.com/msukmanowsky/8086892
     """
     def __init__(self, message=None):
-        self.message = message or u'Field must be an image file.'
+        self.message = message or u'An image file is required'
 
     def __call__(self, form, field):
         if (field.data is None
             or imghdr.what('unused', field.data.read()) is None):
-            message = self.message or 'An image file is required'
-            raise validators.StopValidation(message)
+            raise StopValidation(self.message)
 
         field.data.seek(0)
 
 class ImageManipulationForm(Form):
-    image = FileField('image', validators=[ImageFileRequired()])
+    image = FileField('image',
+        validators=[ImageFileRequired(message='You must upload an image file')])
 
-    animation = RadioField('animation', choices=[('auto', 'GIF'),
-        ('one_frame', 'One Frame'), ('custom', 'Custom')], default='auto')
+    animation = RadioField('animation',
+        choices=[('auto', 'GIF'),
+                 ('one_frame', 'One Frame'),
+                 ('custom', 'Custom')],
+        default='auto')
     box_size = StringField('box_size')
     frames = StringField('frames')
     save_frames = BooleanField('save_frames')
 
-    box_shape = RadioField('box_shape', choices=[('square', 'Square'),
-        ('vertical', 'Vertical slices'), ('horizontal', 'Horizontal slices')],
+    box_shape = RadioField('box_shape',
+        choices=[('square', 'Square'),
+                 ('vertical', 'Vertical slices'),
+                 ('horizontal', 'Horizontal slices')],
         default='square')
 
-    rotation = SelectField('rotation', choices=[('none', 'None'), ('flip', 'Flip'), ('ninety', 'Multiples of 90 degrees')])
+    rotation = SelectField('rotation',
+        choices=[('none', 'None'),
+                 ('flip', 'Flip'),
+                 ('ninety', 'Multiples of 90 degrees')])
     randomize = BooleanField('randomize')
     average = BooleanField('average')
 
